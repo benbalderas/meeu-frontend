f90bf7a2680c6daf0322fbda83cd4c93f9d6f445
Ben Balderas <benbalderas3@gmail.com>
2020-07-10 12:47:43 -0500

    fixed absolute imports, include store in provider

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,2 +1,17 @@
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
+import museums from "redux/MuseumsDuck";
+
+export const rootReducer = combineReducers({
+  museums,
+});
+
+const composeEnhancers =
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPONSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
+
+export default store;
