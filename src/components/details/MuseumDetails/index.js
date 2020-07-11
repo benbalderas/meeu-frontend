01617b18784da9fb4ed57e542bc3dd3e4247ac27
Ben Balderas <benbalderas3@gmail.com>
2020-07-10 20:18:26 -0500

    Start details component

---
+++ src/components/details/MuseumDetails/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { Container, Grid, Typography, Button } from '@material-ui/core';
+import NavBar from 'components/navigation/NavBar';
+
+export default function MuseumDetails() {
+  return (
+    <>
+      <Container>
+        <Grid container>
+          <Grid item></Grid>
+        </Grid>
+      </Container>
+    </>
+  );
+}
