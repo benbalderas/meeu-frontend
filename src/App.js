50ced5b9f116de67ad9356cad91e74c0a94ced67
Ben Balderas <benbalderas3@gmail.com>
2020-07-13 15:53:36 -0500

    Added conditional wrapper to app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { useLocation } from 'react-router-dom';
-import './App.css';
-import theme from 'theme';
 
+import 'App.css';
+import theme from 'theme';
 import { ThemeProvider, CssBaseline } from '@material-ui/core';
 
 import getScreenTitle from 'helpers/router';
+import ConditionalWrapper from 'components/wrappers/ConditionalWrapper';
 import MainNav from 'components/navigation/MainNav';
 import Routes from 'Routes';
 
 function App() {
   const location = useLocation();
+  const user = useSelector((state) => state.user.data);
 
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
+
       <div className="App">
-        <MainNav screenTitle={getScreenTitle(location.pathname)}>
-          <Routes />
-        </MainNav>
+        <ConditionalWrapper
+          condition={user}
+          wrapper={(children) => (
+            <MainNav screenTitle={getScreenTitle(location.pathname)}>
+              {children}
+            </MainNav>
+          )}
+        >
+          <>
+            <Routes />
+          </>
+        </ConditionalWrapper>
       </div>
     </ThemeProvider>
   );
