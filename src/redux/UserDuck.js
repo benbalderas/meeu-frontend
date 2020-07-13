9ee5aa806e39cd43cc846f6746c0f24685fdee33
Ben Balderas <benbalderas3@gmail.com>
2020-07-12 20:02:24 -0500

    Created user duck

---
+++ src/redux/UserDuck.js
@@ -0,0 +1,60 @@
+import axios from 'axios';
+axios.defaults.withCredentials = true; // needed to set cookies
+
+const LOADING = 'meeuapp/user/LOADING';
+const LOGIN_SUCCESS = 'meeuapp/user/LOGIN_SUCCESS';
+const LOGIN_ERROR = 'meeuapp/user/LOGIN_ERROR';
+const LOGOUT = 'meeuapp/user/LOGOUT';
+
+const initialState = {
+  data: undefined,
+  status: '',
+  error: undefined,
+};
+
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+    case LOADING:
+      return { ...state, status: 'pending' };
+    case LOGIN_SUCCESS:
+      return { status: 'success', data: { ...action.payload } };
+    case LOGIN_ERROR:
+      return { status: 'error', error: action.error };
+    case LOGOUT:
+      return initialState;
+    default:
+      return state;
+  }
+}
+
+export const loadingUser = () => ({
+  type: LOADING,
+});
+
+export const loginSuccess = (payload) => ({
+  type: LOGIN_SUCCESS,
+  payload,
+});
+
+export const loginError = (error) => ({
+  type: LOGIN_ERROR,
+  error,
+});
+
+export const logout = () => ({
+  type: LOGOUT,
+});
+
+// pass data to think in firs parameter
+export const login = (credentials) => (dispatch) => {
+  dispatch(loadingUser());
+
+  return axios
+    .post('http://localhost:3000/login', credentials)
+    .then((res) => {
+      dispatch(loginSuccess(res.data.user));
+    })
+    .catch((res) => {
+      dispatch(loginError(res.response.data));
+    });
+};
