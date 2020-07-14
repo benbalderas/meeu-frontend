26a9287a876bb942ca59bd449369a5781723451a
Ben Balderas <benbalderas3@gmail.com>
2020-07-14 18:20:35 -0500

    Theme and styles edits

--- src/theme.js
+++ src/theme.js
@@ -201,4 +201,8 @@ const theme = createMuiTheme({
   },
 });
 
+// Custom shadows
+theme.shadows[6] =
+  'box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05);';
+
 export default theme;
