3331ec2d7c5ede5c8ccc73994bb8f0ba456a44f7
Ben Balderas <benbalderas3@gmail.com>
2020-07-15 15:46:43 -0500

    Added nav bar with close button

--- src/components/forms/CreateExhibit/index.js
+++ src/components/forms/CreateExhibit/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 import {
   Container,
@@ -12,69 +13,83 @@ import {
   Button,
 } from '@material-ui/core';
 
+import CloseIcon from '@material-ui/icons/Close';
+import NavBar from 'components/navigation/NavBar';
+
 export default function CreateExhibit() {
+  const { push, goBack } = useHistory();
   const [type, setType] = useState('');
 
   const handleChange = (event) => {
     setType(event.target.value);
   };
 
+  const handleBackClick = () => {
+    push('/exhibits');
+  };
+
   return (
-    <Box mt={3} mb={5}>
-      <Container maxWidth="xs">
-        <Typography gutterBottom variant="h2">
-          New Exhibit
-        </Typography>
+    <>
+      <NavBar screenTitle={null} onClick={handleBackClick}>
+        <CloseIcon />
+      </NavBar>
 
-        <form>
-          <TextField
-            fullWidth
-            name="title"
-            id="title"
-            label="Exhibit Title"
-            type="text"
-            required
-          />
+      <Box mt={3} mb={5}>
+        <Container maxWidth="xs">
+          <Typography gutterBottom variant="h2">
+            New Exhibit
+          </Typography>
 
-          <TextField
-            fullWidth
-            name="description"
-            id="desc"
-            label="Description"
-            type="text"
-            multiline
-            rows={4}
-            required
-          />
-
-          <FormControl fullWidth required name="type">
-            <InputLabel id="type">Type</InputLabel>
-
-            <Select labelId="type" value={type} onChange={handleChange}>
-              <MenuItem value="Permanent">Permanent</MenuItem>
-              <MenuItem value="Temporary">Temporary</MenuItem>
-            </Select>
-          </FormControl>
-
-          {type === 'Temporary' && (
+          <form>
             <TextField
               fullWidth
-              name="endDate"
-              id="date"
-              label="End Date"
-              type="date"
-              defaultValue="2020-08-24"
-              InputLabelProps={{
-                shrink: true,
-              }}
+              name="title"
+              id="title"
+              label="Exhibit Title"
+              type="text"
+              required
             />
-          )}
 
-          <Button fullWidth variant="contained" color="primary" type="submit">
-            Create
-          </Button>
-        </form>
-      </Container>
-    </Box>
+            <TextField
+              fullWidth
+              name="description"
+              id="desc"
+              label="Description"
+              type="text"
+              multiline
+              rows={4}
+              required
+            />
+
+            <FormControl fullWidth required name="type">
+              <InputLabel id="type">Type</InputLabel>
+
+              <Select labelId="type" value={type} onChange={handleChange}>
+                <MenuItem value="Permanent">Permanent</MenuItem>
+                <MenuItem value="Temporary">Temporary</MenuItem>
+              </Select>
+            </FormControl>
+
+            {type === 'Temporary' && (
+              <TextField
+                fullWidth
+                name="endDate"
+                id="date"
+                label="End Date"
+                type="date"
+                defaultValue="2020-08-24"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+            )}
+
+            <Button fullWidth variant="contained" color="primary" type="submit">
+              Create
+            </Button>
+          </form>
+        </Container>
+      </Box>
+    </>
   );
 }
