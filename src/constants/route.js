26849c42aa837f1dba25ac9f05df25c3dc1f4a26
Ben Balderas <benbalderas3@gmail.com>
2020-07-16 12:45:29 -0500

    Add www to base url

--- src/constants/route.js
+++ src/constants/route.js
@@ -1,7 +1,7 @@
 const isProduction = process.env.NODE_ENV === 'production';
 
 const base_url = isProduction
-  ? 'https://meeu.app/api'
+  ? 'https://www.meeu.app/api'
   : 'http://localhost:3000/api';
 
 export default base_url;
