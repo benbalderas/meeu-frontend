8ff2f9ce59b4e0716c6f248c80d6566efd981a61
Ben Balderas <benbalderas3@gmail.com>
2020-07-16 14:44:06 -0500

    Added date-fns package

--- src/components/details/ExhibitDetails/index.js
+++ src/components/details/ExhibitDetails/index.js
@@ -17,8 +17,38 @@ import {
 } from '@material-ui/core';
 import KeyboardBackspaceOutlinedIcon from '@material-ui/icons/KeyboardBackspaceOutlined';
 import NavBar from 'components/navigation/NavBar';
+import ArtworkCard from 'components/cards/ArtworkCard';
+
+const useStyles = makeStyles((theme) => ({
+  metadata: {
+    '& > *:not(:first-child)': {
+      marginLeft: theme.spacing(1),
+    },
+  },
+}));
 
 export default function ExhibitDetails() {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const classes = useStyles();
+  const { id } = useParams();
+
+  // State selectors
+  const exhibit = useSelector((state) => state.exhibits.items);
+  const artworks = useSelector((state) =>
+    denormalizeData(state.artworks.items)
+  );
+
+  useEffect(() => {
+    dispatch(fetchSingleExhibit(id));
+    dispatch(fetchArtworks(id));
+  }, [dispatch, id]);
+
+  // Handlers
+  const handleBackClick = () => {
+    history.goBack();
+  };
+
   return (
     <>
       <NavBar screenTitle="" onClick={handleBackClick}>
@@ -28,11 +58,21 @@ export default function ExhibitDetails() {
       <Container maxWidth="lg">
         <Grid container spacing={4} alignItems="center">
           <Grid item md={6} lg={6} sm={12}>
-            <Typography variant="h4">Exhibition Name</Typography>
+            <Typography variant="h4">{exhibit.title}</Typography>
 
-            <Box display="flex" alignItems="center">
-              <Chip variant="outlined" size="small" label="Temporary" />
-              <Typography variant="h4">Until: Date</Typography>
+            <Box
+              className={classes.metadata}
+              mt={3}
+              display="flex"
+              alignItems="center"
+            >
+              <Chip
+                variant="outlined"
+                size="small"
+                label={exhibit.type}
+                color={exhibit.type === 'Temporary' ? 'primary' : 'default'}
+              />
+              <Chip variant="outlined" size="small" label={exhibit.endDate} />
             </Box>
 
             <Typography variant="body2">Description</Typography>
