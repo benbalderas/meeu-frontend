433800e4d3a81a35a8793d210a6efc4eca76fd2a
Ben Balderas <benbalderas3@gmail.com>
2020-07-20 18:56:52 -0500

    Fix avatar preview

--- src/components/details/UserDetails/index.js
+++ src/components/details/UserDetails/index.js
@@ -61,13 +61,11 @@ export default function UserDetails() {
     const value = event.target.value;
 
     setUserData((prevState) => ({ ...prevState, [key]: value }));
-
-    if (event.target.name === 'avatar')
-      setFilePreview(URL.createObjectURL(event.target.files[0]));
   };
 
   const handleAvatarChange = (event) => {
     setAvatar(event.target.files);
+    setFilePreview(URL.createObjectURL(event.target.files[0]));
   };
 
   const handleSubmit = (event) => {
@@ -75,13 +73,6 @@ export default function UserDetails() {
     const formData = new FormData();
 
     for (let key in userData) {
-      //   if (key === 'avatar' && Array.isArray([])) {
-      //     //   formData.append(key, userData[key][0]);
-      //     console.log('its array:', userData[key][0]);
-      //   } else {
-      //     formData.append(key, userData[key]);
-      //     console.log('its not array');
-      //   }
       formData.append(key, userData[key]);
     }
 
@@ -89,12 +80,15 @@ export default function UserDetails() {
       formData.append('avatar', avatar[0]);
     }
 
-    dispatch(userUpdate(formData, user._id));
-    setNotif(true);
+    dispatch(userUpdate(formData, user._id, setNotif));
   };
 
   const handleBackClick = () => {
     history.goBack();
+  };
+
+  const handleClose = (event) => {
+    setNotif(false);
   };
 
   return (
@@ -186,7 +180,8 @@ export default function UserDetails() {
           horizontal: 'right',
         }}
         open={notif}
-        autoHideDuration={6000}
+        onClose={handleClose}
+        autoHideDuration={4000}
         message="Cool, profile updated."
       />
     </>
