59a85f8e82d8bab50bcc2fa4056c9c2eaff392fd
Ben Balderas <benbalderas3@gmail.com>
2020-07-21 17:28:14 -0500

    fix wrong menu showing on signup

--- src/components/navigation/LandingNav/index.js
+++ src/components/navigation/LandingNav/index.js
@@ -40,7 +40,7 @@ export default function LandingNav({ children }) {
 
           <Box className={classes.slogan} />
 
-          {user ? (
+          {Object.keys(user).length > 0 ? (
             <Button
               component={NavLink}
               to="/museums"
