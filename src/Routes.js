d3200413dd921e790f7158af6164fa65cb36696c
Ben Balderas <benbalderas3@gmail.com>
2020-07-23 22:17:54 -0500

    Reorder

--- src/Routes.js
+++ src/Routes.js
@@ -4,12 +4,6 @@ import { Route, Switch } from 'react-router-dom';
 import PrivateRoute from 'components/wrappers/PrivateRoute';
 import AdminRoute from 'components/wrappers/AdminRoute';
 
-// Landing
-import LandingNav from 'components/navigation/LandingNav';
-import LandingPage from 'components/pages/LandingPage';
-import Login from 'components/forms/Login';
-import Signup from 'components/forms/Signup';
-
 // Grids
 import MuseumGrid from 'components/grids/MuseumGrid';
 import ExhibitsGrid from 'components/grids/ExhibitsGrid';
@@ -24,6 +18,12 @@ import MuseumDetails from 'components/details/MuseumDetails';
 import ExhibitDetails from 'components/details/ExhibitDetails';
 import ArtworkDetails from 'components/details/ArtworkDetails';
 import UserDetails from 'components/details/UserDetails';
+
+// Landing
+import LandingNav from 'components/navigation/LandingNav';
+import LandingPage from 'components/pages/LandingPage';
+import Login from 'components/forms/Login';
+import Signup from 'components/forms/Signup';
 
 const pageFadeIn = {
   in: {
@@ -43,38 +43,6 @@ const AnimationRoute = ({ children }) => (
 const Routes = () => (
   <AnimatePresence exitBeforeEnter>
     <Switch>
-      {/* Landing */}
-      <Route
-        exact
-        path="/"
-        component={() => (
-          <>
-            <LandingNav />
-            <LandingPage />
-          </>
-        )}
-      />
-      <Route
-        exact
-        path="/login"
-        component={() => (
-          <>
-            <LandingNav />
-            <Login />
-          </>
-        )}
-      />
-      <Route
-        exact
-        path="/signup"
-        component={() => (
-          <>
-            <LandingNav />
-            <Signup />
-          </>
-        )}
-      />
-
       {/* Grids */}
       <PrivateRoute
         exact
@@ -159,6 +127,38 @@ const Routes = () => (
           <AnimationRoute>
             <UserDetails />
           </AnimationRoute>
+        )}
+      />
+
+            {/* Landing */}
+            <Route
+        exact
+        path="/"
+        component={() => (
+          <>
+            <LandingNav />
+            <LandingPage />
+          </>
+        )}
+      />
+      <Route
+        exact
+        path="/login"
+        component={() => (
+          <>
+            <LandingNav />
+            <Login />
+          </>
+        )}
+      />
+      <Route
+        exact
+        path="/signup"
+        component={() => (
+          <>
+            <LandingNav />
+            <Signup />
+          </>
         )}
       />
     </Switch>
